8279f6b0c669a4b02e8f165a8161ef1ed247086e
shyam-vora <shyamvora2310@gmail.com>
2023-05-13 16:23:02 +0530

    change

--- p1233.java
+++ p1233.java
@@ -42,47 +42,4 @@ public class p1233 {
 		d1.deliver();
     }
 }
-// public class AnimalTransportTest {
-//     public static void main(String[] args) 
-// 	{
-//         Animal[] animals = {new Tiger(), new Camel(), new Deer(), new Donkey()};
-        
-//         for (Animal animal : animals) {
-//             if (animal instanceof Transport) {
-//                 Transport transportAnimal = (Transport) animal;
-//                 transportAnimal.deliver();
-//             }
-//         }
-//     }
-// }
-
-// interface Transport {
-//     void deliver();
-// }
-
-// abstract class Animal {
-//     // class implementation omitted for brevity
-// }
-
-// class Tiger extends Animal {
-//     // class implementation omitted for brevity
-// }
-
-// class Camel extends Animal implements Transport {
-//     @Override
-//     public void deliver() {
-//         System.out.println("Camel delivering goods");
-//     }
-// }
-
-// class Deer extends Animal {
-//     // class implementation omitted for brevity
-// }
-
-// class Donkey extends Animal implements Transport {
-//     @Override
-//     public void deliver() {
-//         System.out.println("Donkey delivering goods");
-//     }
-// }
 
